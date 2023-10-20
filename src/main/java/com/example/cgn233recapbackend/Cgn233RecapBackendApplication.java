505b2105efdc90eadd3956d6dee187d004ba4901
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-20 14:34:26 +0200

    Initial commit /w resources

---
+++ src/main/java/com/example/cgn233recapbackend/Cgn233RecapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgn233recapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn233RecapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn233RecapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgn233recapbackend/Cgn233RecapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgn233recapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn233RecapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
